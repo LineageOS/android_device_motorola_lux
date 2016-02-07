aca8207fb728a04be68f346b004579b33799fa80
Sultan Qasim Khan <sultanqasim@gmail.com>
2016-02-07 01:49:51 -0500

    lux: use prebuilt audio HAL
    Motorola changed too much, and I don't have time to reverse engineer,
    recreate, and make an open source HAL work. So for now, I just give
    up and will use the prebuilt HAL.
    
    Change-Id: Ifb412c77bcab9694c316c88da5b0a0dab22341bc

--- device.mk
+++ device.mk
@@ -19,7 +19,8 @@ $(call inherit-product, device/motorola/msm8916-common/msm8916.mk)
 # Audio
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml \
-    $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml
+    $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml \
+    $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths_florida.xml
 
 # Media
 PRODUCT_COPY_FILES += \
