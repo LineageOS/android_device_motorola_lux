553f50bf22aaa115965320f2ee198f95ca4fd3d4
Kevin F. Haggerty <haggertk@lineageos.org>
2018-09-20 08:19:36 -0600

    camera: Set SDK API level for required executables
    * This singularly handles both text relocation (M) and mutex (P)
      linker restrictions
    * cameraserver - Needs text relocations
    * mediaserver - Needs text relocations
    * mm-qcamera-daemon - Needs both text relocations and mutex restrictions
      relaxed
    
    Change-Id: I97a79fb57aaadf5cf62894489522b2de6d8c2e36

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -24,7 +24,9 @@ TARGET_OTA_ASSERT_DEVICE := lux,lux_uds,xt1562,xt1563
 
 # Camera
 TARGET_PROCESS_SDK_VERSION_OVERRIDE := \
-	/system/bin/mm-qcamera-daemon=25
+    /system/bin/cameraserver=25 \
+    /system/bin/mediaserver=25 \
+    /system/bin/mm-qcamera-daemon=25
 
 # HIDL Manifest
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
