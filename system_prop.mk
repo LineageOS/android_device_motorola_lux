ec1782b649ae46dde31d959e1098a9410c59cbaf
Rahul Krishna <Rahulkrishna585@gmail.com>
2020-09-09 22:07:57 +0530

    lux: Disable GPU protected composition
    Change-Id: I54c40e9ba876c88185afbdbfa586e5f988741657

--- system_prop.mk
+++ system_prop.mk
@@ -20,7 +20,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # SurfaceFlinger
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    ro.surface_flinger.protected_contents=true \
     ro.surface_flinger.vsync_event_phase_offset_ns=4000000 \
     ro.surface_flinger.vsync_sf_event_phase_offset_ns=8000000 \
     ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
