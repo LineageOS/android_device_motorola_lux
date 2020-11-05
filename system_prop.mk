958563ea52dd7d427856abfa1d3923a402bd4089
Sourajit Karmakar <sourajit.karmakar@gmail.com>
2020-11-05 06:35:21 +0100

    lux: Drop all SF offset related props.
    For triple frame buffers 19db91f2cfc71b5cd924268b5c40bf329fdc6efd to work properly, these props
    need to be dropped to prevent any unneeded regressions.
    
    Change-Id: I058cef2c28af1d6512d88f7fc5e840c0aaedbd01

--- system_prop.mk
+++ system_prop.mk
@@ -19,8 +19,4 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # SurfaceFlinger
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    ro.surface_flinger.protected_contents=true \
-    ro.surface_flinger.vsync_event_phase_offset_ns=4000000 \
-    ro.surface_flinger.vsync_sf_event_phase_offset_ns=8000000 \
-    ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
-    ro.surface_flinger.max_virtual_display_dimension=2048
+    ro.surface_flinger.protected_contents=true
