d3dfe3497fa77267d2f60b4810a3dcf9c503863c
Jeferson <jroliveira.oliveira301@gmail.com>
2022-02-14 12:38:23 +0100

    lux: Build common MotoActions
    Change-Id: Ibef2d7e154da0f1581dfbc41bbfe11fb41938aae

--- device.mk
+++ device.mk
@@ -44,6 +44,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media_codecs_performance.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_performance.xml \
     $(LOCAL_PATH)/configs/media_profiles_V1_0.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_profiles_V1_0.xml
 
+# MotoActions
+PRODUCT_PACKAGES += \
+    MotoActions
+
 # NFC
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.nfc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.xml \
